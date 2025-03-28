6ae9aed98fe958205115c259e7184cc5ae8d812c
Christian F <68164706+Krill1302@users.noreply.github.com>
2025-03-28 09:44:02 +0100

    Add project files.

---
+++ Code-Challange1/MysteryBox.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challange1
+{
+    public class MysteryBox
+    {
+
+        string _label;
+        string _contains;
+        bool _isExplosive;
+
+
+        public string Label
+        {
+            get { return _label; }
+            set { _label = value; }
+        }
+        public string Contains
+        {
+            get { return _contains; }
+            set { _contains = value; }
+        }
+        public bool IsExplosive
+        {
+            get { return _isExplosive; }
+            set { _isExplosive = value; }
+        }
+
+
+        public MysteryBox(string label, string contains, bool isExplosive) 
+        {
+            _label = label;
+            _contains = contains;
+            _isExplosive = isExplosive;
+        
+        
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Label}, {Contains}, {IsExplosive}:";
+        }
+
+    }
+}
---
+++ Code-Challange1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Code_Challange1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MysteryBox mysteryBox = new MysteryBox("Code-Challange","slim",true);
+
+            Console.WriteLine(mysteryBox);
+
+        }
+    }
+}
